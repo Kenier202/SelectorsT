587abaf3d1a16d5b9bc0ff17d97de8fa43185e8d
kenier <kenier202@gmail.com>
2024-11-26 10:22:04 -0500

    feat(selector-page): Refine region change handling to fetch countries dynamically using switchMap for improved responsiveness

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CountryModule } from './country/country.module';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -13,6 +14,7 @@ import { CountryModule } from './country/country.module';
     BrowserModule,
     AppRoutingModule,
     CountryModule,
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/country/pages/selector-page/selector-page.component.ts
+++ src/app/country/pages/selector-page/selector-page.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { CountriesService } from '../../services/countries.service';
 import { Region } from '../../interface/country.interface';
+import { switchMap } from 'rxjs';
 
 @Component({
   selector: 'app-selector-page',
@@ -29,8 +30,11 @@ export class SelectorPageComponent implements OnInit{
   }
 
   onRegionChange():void{
-    this.myForm.get('region')!.valueChanges.subscribe(value => {
-      console.log(value);
-    });
+    this.myForm.get('region')!.valueChanges
+    .pipe(switchMap(
+      region => this.countriesService.getCountriesByRegion(region)
+    )).subscribe(
+      region => console.log(region)
+    );
   }
 }
